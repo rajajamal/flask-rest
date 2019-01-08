d4a65d57d0dc0e1d379f029fedf44f5737f69cc2
rajajamal <aceh.rajajamal@gmail.com>
2019-01-08 12:56:38 +0700

    fix config.py

--- myapi/config.py
+++ myapi/config.py
@@ -7,7 +7,7 @@ Use env var to override
 DEBUG = True
 SECRET_KEY = "changeme"
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///' + os.path.join(basedir, 'app.db')"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///' + os.path.join(basedir, 'myapi.db')"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 JWT_BLACKLIST_ENABLED = True
